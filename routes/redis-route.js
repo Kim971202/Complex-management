ddeed31be27700bcd9b1fec645f2bf8fcf2a2761
Kim971202 <kimyohan971202@gmail.com>
2022-09-05 09:21:27 +0900

    redis 키 스캔 관련 function 추가

--- routes/redis-route.js
+++ routes/redis-route.js
@@ -2,15 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 let value = "";
-
 const redis = require("redis");
 const redisInfo = {
   host: "127.0.0.1",
   port: 6379,
   db: 0,
 };
-const client = redis.createClient(redisInfo);
 
+const redisScan = require("node-redis-scan");
+const client = redis.createClient(redisInfo);
+const scanner = new redisScan(client);
 client.on("error", (err) => {
   console.error(err);
 });
@@ -19,12 +20,20 @@ client.on("ready", () => {
   console.log("redis is ready");
 });
 
+scanner.scan("*", (err, matchingKeys) => {
+  if (err) throw err;
+  console.log(matchingKeys);
+});
+
+let a = ["one_key", "myKey", "two_key", "three_key"];
 router.get("/", (req, res, next) => {
   try {
-    client.get("k_two", (err, result) => {
-      console.log("result: " + result); // v_two
-      value = result;
-    });
+    for (i = 0; i < a.length; ++i) {
+      client.get(a[i], (err, result) => {
+        console.log("result: " + result); // v_two
+        value = result;
+      });
+    }
 
     let jsonResult = {
       value,
