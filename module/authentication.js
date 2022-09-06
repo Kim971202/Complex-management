abccc445bee810dcb1b333bfe9ad79f29dd3b038
Kim971202 <kimyohan971202@gmail.com>
2022-09-06 17:16:55 +0900

    redis에서 serviceKey값을 인증하는 fucntion 생성(지금은 null이면 에러 발생)

--- module/authentication.js
+++ module/authentication.js
@@ -5,4 +5,32 @@
  * 파일설명: Redis의 key & value 속성을 이용하여 사용자의 servicekey를 인증하는 스크립트 이다.
  */
 
-function checkServiceKey() {}
+const redis = require("redis");
+const redisInfo = {
+  host: "127.0.0.1",
+  port: 6379,
+  db: 0,
+};
+
+const client = redis.createClient(redisInfo);
+client.on("error", (err) => {
+  console.error(err);
+});
+
+client.on("ready", () => {
+  console.log("redis is ready");
+});
+
+function checkServiceKey(serviceKey) {
+  let isKeyValid = false;
+
+  client.get(serviceKey, (error, result) => {
+    console.log(result);
+    serviceKey = result;
+  });
+  if (result == "") {
+    throw new Error("Invalid ServiceKey has accessed");
+  }
+}
+
+checkServiceKey("wrongServiceKey");
